2c097b0f15b8407548858c6db596729669afa34a
Sergey Populov <sergey@saritasa.com>
2017-06-08 11:51:51 +0500

    Fixes and improvements for CursorResultAuto

--- src/Paging/CursorResultAuto.php
+++ src/Paging/CursorResultAuto.php
@@ -2,6 +2,7 @@
 
 namespace Saritasa\DingoApi\Paging;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 
 /**
@@ -9,20 +10,65 @@ use Illuminate\Support\Collection;
  */
 class CursorResultAuto extends CursorResult
 {
-    function __construct(CursorRequest $cursorRequest, Collection $items, $prev = null, $next = null, $count = null)
+    /**
+     * Cursor result, that tries to automatically detect next, previous cursor values
+     *
+     * @param CursorRequest $cursorRequest Original cursor request
+     * @param Collection $items current page items to return
+     * @param mixed $prev Cursor value for previous page
+     *        (try to detect automatically, based on current cursor value and page size if null)
+     * @param mixed $next Cursor value for next page (try to detect automatically as last item ID, if null)
+     */
+    function __construct(CursorRequest $cursorRequest, Collection $items, $prev = null, $next = null)
     {
         $current = $cursorRequest->current ?: $this->getKeyOrNull($items->first());
         $count = $items->count();
 
-        if ($next == null && is_int($current) && $count >= $cursorRequest->pageSize) {
+        if ($next == null && $count >= $cursorRequest->pageSize) {
             $next = $this->getKeyOrNull($items->last());
         }
-        if ($prev == null && is_int($current)) {
-            $prev = $current - $cursorRequest->pageSize;
+
+        if ($this->isIntegerKey($current, $items->first())) {
+            $current = (int)$cursorRequest->current;
+            if ($next != null) {
+                $next = (int)$next;
+            }
+            $prev = $current ? $current - $cursorRequest->pageSize : null;
             if ($prev < 0) {
                 $prev = null;
             }
         }
+
         parent::__construct($cursorRequest, $items, $prev, $next, $count);
+    }
+
+    /**
+     * Try to get model key
+     */
+    protected function getKeyOrNull($model)
+    {
+        if ($model != null && $model instanceof Model) {
+            return $model->getKey();
+        }
+        return null;
+    }
+
+    /**
+     * Detect, if cursor value is integer or not.
+     * Extract key type from model, if possible, otherwise just check cursor value type
+     *
+     * @param mixed $current Current curor value
+     * @param mixed $model one of items or null
+     * @return bool
+     */
+    protected function isIntegerKey($current, $model): bool {
+        if ($model != null && $model instanceof Model) {
+            return $model->getKeyType() == 'int';
+        } else {
+            if ($current != null && is_numeric($current) || ctype_digit($current)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
