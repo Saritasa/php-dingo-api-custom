53c6d05b64be18cbaf1bb2ff75615acbd8f1871b
VladimirBerdnik <v.berdnik@saritasa.com>
2018-03-12 23:47:42 +0600

    Resolve issue with CursorRequest initialisation
    When issue was thrown when not all configuration parameters
    was passed into constructor

--- src/Paging/CursorRequest.php
+++ src/Paging/CursorRequest.php
@@ -6,10 +6,11 @@ use Saritasa\Exceptions\PagingException;
 use Illuminate\Contracts\Support\Arrayable;
 
 /**
- * Read-only paging cursor description.
+ * Cursor-type pagination information.
+ * Contains details about items count in page and to which item this cursor points to.
  *
- * @property-read int $pageSize
- * @property-read int $current
+ * @property-read int $pageSize Number of items in current cursor-type page
+ * @property-read int $current Position of item from which this cursor starts. Zero-based.
  */
 class CursorRequest implements Arrayable
 {
@@ -25,18 +26,25 @@ class CursorRequest implements Arrayable
     /* @var int */
     private $current;
 
+    /**
+     * Cursor-type pagination information.
+     * Contains details about items count in page and to which item this cursor points to.
+     *
+     * @param array|null $input Associated array with key-value pairs of cursor configuration
+     * @see CursorRequest::KEYS for valid configuration keys
+     */
     public function __construct(array $input = null)
     {
+        $this->current = 0;
         $this->pageSize = config('api.defaultLimit');
+
         if ($input) {
             if (isset($input[static::CURRENT])) {
                 $this->current = ctype_digit($input[static::CURRENT])
                     ? (int)$input[static::CURRENT]
                     : $input[static::CURRENT];
-            } else {
-                $this->current = 0;
             }
-            if ($input[static::PAGE_SIZE]) {
+            if (isset($input[static::PAGE_SIZE])) {
                 $this->pageSize = $input[static::PAGE_SIZE];
             }
         }
